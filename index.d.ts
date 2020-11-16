fbb369e56a913f6191b109d895fdea38029646e4
seiji-lash <72863853+seiji-lash@users.noreply.github.com>
2020-11-16 16:34:11 +0800

    Remove async
    Fix  error TS1040: 'async' modifier cannot be used in an ambient context.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /** Returns true if given parameter is local IP address or resolves into one, false otherwise */
-declare async function isLocalhost(
+declare function isLocalhost(
   addrOrHost: string,
   canBind?: boolean,
 ): Promise<boolean>;
